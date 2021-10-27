36cbf11275f244e50f2a3c30a5d41e695d88ded4
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-10-27 15:55:51 +0200

    Use PDO for the App Database

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -3,12 +3,49 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\View;
+use PDO;
+
 
 class HomeController
 {
     public function index():View
     {
-        return View::make('index', ['foo'=>'bar']);
+        try {
+            $db = new PDO('mysql:host=db;dbname=my_db', 'root', 'root');
+
+            $email='hadi@gmail.com';
+            $name='hadi';
+            $isActive=1;
+            $createdAt=date('Y-m-d H:i:s', strtotime('10/11/2021 12:34PM'));
+
+            $query='INSERT INTO user (email, full_name, is_active, created_at) 
+                  VALUES (:email, :name, :active, :date)';
+            $stmt=$db->prepare($query);
+            $stmt->bindParam('name', $name);
+            $stmt->bindParam('email', $email);
+            $stmt->bindParam('active', $isActive, PDO::PARAM_BOOL);
+            $stmt->bindParam('date', $createdAt);
+            $isActive=0;
+
+            $stmt->execute();
+
+
+            $id=(int) $db->lastInsertId();
+
+
+            $users=$db->query('SELECT * FROM user WHERE id='.$id)->fetch();
+
+            foreach($users as $user){
+                echo '<pre>';
+                var_dump($user);
+                echo '<pre>';
+            }
+
+
+        }catch (\PDOException $e){
+            throw new \PDOException($e->getMessage(), (int) $e->getCode());
+        }
+        return View::make('index');
     }
 
     public function download()
